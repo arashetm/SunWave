e7ceba850bed11b87da038b14ee8a33c3a7a08d4
Arash Etemad <arashetemad94@yahoo.com>
2023-11-11 19:47:19 +0330

    Initial Commit

---
+++ SunWave/SunWaveApp.swift
@@ -0,0 +1,17 @@
+//
+//  SunWaveApp.swift
+//  SunWave
+//
+//  Created by Arash on 11/11/23.
+//
+
+import SwiftUI
+
+@main
+struct SunWaveApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
